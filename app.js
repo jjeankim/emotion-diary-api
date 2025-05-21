b2c1d96d7a5cf97251bade6667304b69671caa0c
김진 <jjean@nambu09ui-MacBookPro.local>
2025-05-21 12:10:56 +0900

    Initial commit & api 생성

---
+++ app.js
@@ -0,0 +1,61 @@
+import express from "express";
+import diaryList from "./mock.js";
+
+const app = express();
+
+//req바디를 json객체로 변환
+app.use(express.json());
+
+app.get("/diary", (req, res) => {
+  res.send(diaryList);
+});
+
+app.get("/diary/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const diaryItem = diaryList.find((diary) => diary.id === id);
+  if (diaryItem) res.send(diaryItem);
+  else {
+    res.status(404).send({ message: "Cannot find given id." });
+  }
+});
+
+const getNextId = (arr) => {
+  const ids = arr.map((el) => el.id);
+  return Math.max(...ids) + 1;
+};
+
+app.post("/diary", (req, res) => {
+  const newDiary = req.body;
+  newDiary.id = getNextId(diaryList);
+  newDiary.createdAt = new Date();
+  newDiary.updatedAt = new Date();
+  diaryList.push(newDiary);
+  res.status(201).send(newDiary);
+});
+
+app.patch("/diary/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const diaryItem = diaryList.find((diary) => diary.id === id);
+  if (diaryItem) {
+    Object.keys(req.body).forEach((key) => {
+      diaryItem[key] = req.body[key];
+    });
+    diaryItem.updatedAt = new Date();
+    res.send(diaryItem);
+  } else {
+    res.status(404).send({ "message": "Cannot find given id" });
+  }
+});
+
+app.delete("/diary/:id",(req,res) => {
+  const id = Number(req.params.id)
+  const idx = diaryList.findIndex(item => item.id === id)
+  if (idx >= 0) {
+    diaryList.splice(idx, 1)
+    res.sendStatus(204)
+  } else {
+    res.status(404).send({"message": "Cannot find given id."})
+  }
+})
+
+app.listen(4000, () => console.log("Server Started!"));
---
+++ mock.js
@@ -0,0 +1,32 @@
+const diaryList = [
+  {
+    id: 1,
+    title: "1일의 일기",
+    content: "정말 더운 날이다.",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "2일의 일기",
+    content: "비오는 날이다.",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 3,
+    title: "3일의 일기",
+    content: "바람이 많이 부는 날이다.",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 4,
+    title: "4일의 일기",
+    content: "열대야.",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export default diaryList
